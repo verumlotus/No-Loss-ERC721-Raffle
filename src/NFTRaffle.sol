094d22c13520441593c4b675e6972d9cfa5ceaf8
verumlotus <universumtremendum@gmail.com>
2022-05-26 13:21:19 -0400

    Add chainlink git submodule and remapping

--- src/NFTRaffle.sol
+++ src/NFTRaffle.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+
 /**
- * @title title
- * @notice description
+ * @title No-loss NFT Raffle
+ * @notice No-loss NFT raffle that deposits user funds into a yield-bearing strategy and forwards all interest to NFT creator.
  * @author verum
  */
-contract Contract {
+contract NFTRaffle {
     /************************************************
      *  STORAGE
      ***********************************************/
