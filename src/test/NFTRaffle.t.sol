b3e912d0d35ff519d839e7a47cc045264100a6b3
verumlotus <universumtremendum@gmail.com>
2022-05-26 16:00:22 -0400

    NFTRaffleTest

--- src/test/NFTRaffle.t.sol
+++ src/test/NFTRaffle.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.11;
 
 import "./base/BaseTest.sol";
 
-contract ContractTest is BaseTest {
+contract NFTRaffleTest is BaseTest {
     function setUp() public {}
 
     function testExample() public {
